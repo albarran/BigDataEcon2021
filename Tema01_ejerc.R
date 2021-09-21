84d60a02d2d7579f522cf54dfaea65f40c54f357
albarran <albarran@ua.es>
2021-09-21 17:40:20 +0200

    Tema 01 Ejerc

---
+++ Tema01_ejerc.R
@@ -0,0 +1,130 @@
+############################################################
+## Ejercicio Tema 01
+## NOMBRE: (reemplaza èso con tu Nombre y Apellido(s))
+## DNI:    (reemplaza esto con tu DNI, o similar)
+############################################################
+
+
+###---------- Apartado 1.a) ----------------------------------------------------
+
+## escribe tus propios pasos aqui
+## no olvides hacer comentarios si lo consideras necesario
+
+
+
+vector_dias
+
+###---------- Apartado 1.b) ----------------------------------------------------
+
+
+Ventas_totales_semana
+
+
+Ventas_media_A
+
+###---------- Apartado 1.c) ----------------------------------------------------
+
+Ventas_A_entre_semana
+
+
+Ventas_seleccion
+
+
+dias_Ventas_seleccion
+
+
+###---------- Apartado 2.a) ----------------------------------------------------
+
+
+matriz_ventas
+
+###---------- Apartado 2.b) ----------------------------------------------------
+
+
+matriz_ventas_combinada
+
+###---------- Apartado 2.c) ----------------------------------------------------
+
+
+matriz_ventas_combinada_total
+
+###---------- Apartado 2.d) ----------------------------------------------------
+
+
+internac_media
+
+
+nacional_seleccion_media
+
+###---------- Apartado 2.e) ----------------------------------------------------
+
+
+compradores_estimados
+
+# Matriz de precios
+precios_matriz <- matrix(c(5, 5, 6, 6, 7, 7, 4, 4, 4.5, 4.5, 4.9, 4.9), nrow = 6, byrow = TRUE, 
+                         dimnames = list(c(nombres,nombres2), region))
+
+
+
+compradores
+
+###---------- Apartado 3.a) ----------------------------------------------------
+
+
+factor_satisf_vector
+
+###---------- Apartado 3.b) ----------------------------------------------------
+
+print(sum_vector)
+
+print(sum_factor)
+
+###---------- Apartado 3.c) ----------------------------------------------------
+
+
+comparar_clientes
+
+
+###---------- Apartado 4.a) ----------------------------------------------------
+
+# Mostrar la estructura del conjunto de datos
+str(mtcars)
+
+# Mostrar las primeras filas (observaciones)
+head(mtcars)
+
+
+Datsun710_CV
+
+Valiant_vector
+
+###---------- Apartado 4.b) ----------------------------------------------------
+
+cilindros
+
+cambio_vector
+
+###---------- Apartado 4.c) ----------------------------------------------------
+
+
+media_consumo_autom 
+
+
+media_consumo_autom_cyl4
+
+
+###---------- Apartado 5.a) ----------------------------------------------------
+
+
+# Estructura de la lista
+str(mi_lista)
+
+mi_lista
+
+###---------- Apartado 5.b) ----------------------------------------------------
+
+
+media_vector
+
+media_datos_5
